890a7bf174ea7446530a89313fd68744597d7bdd
amnah5 <amooon_96@hotmail.com>
2021-12-04 00:56:06 +0300

    profile

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -3,15 +3,17 @@ import React from "react";
 import Signin from "../Authentication/Signin";
 import Signup from "../Authentication/Signup";
 import Home from "../Pages/Home";
+import Profile from "../user/Profile"
 
 const RootNavigator = () => {
   const { Navigator, Screen } = createStackNavigator();
 
   return (
-    <Navigator initialRouteName="Home">
+    <Navigator initialRouteName="Profile">
       <Screen name="Signin" component={Signin} />
       <Screen name="Signup" component={Signup} />
       <Screen name="Home" component={Home} />
+    <Screen name="Profile" component={Profile}/>
     </Navigator>
   );
 };
