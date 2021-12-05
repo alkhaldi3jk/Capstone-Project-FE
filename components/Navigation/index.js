4fce2e64ae125756cdb9867b71dbd7f2f25eb63f
salmashaan <smalmashaan@gmail.com>
2021-12-05 19:43:28 +0300

    fetch works

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -2,18 +2,20 @@ import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 import Signin from "../Authentication/Signin";
 import Signup from "../Authentication/Signup";
-import Home from "../Pages/Home";
-import Profile from "../user/Profile"
+import Home from "../Pages/home";
+import ServiceDetail from "../Pages/ServiceDetail";
+import Profile from "../user/Profile";
 
 const RootNavigator = () => {
   const { Navigator, Screen } = createStackNavigator();
 
   return (
-    <Navigator initialRouteName="Profile">
+    <Navigator initialRouteName="Home">
       <Screen name="Signin" component={Signin} />
       <Screen name="Signup" component={Signup} />
       <Screen name="Home" component={Home} />
-    <Screen name="Profile" component={Profile}/>
+      <Screen name="Profile" component={Profile} />
+      <Screen name="ServiceDetail" component={ServiceDetail} />
     </Navigator>
   );
 };
