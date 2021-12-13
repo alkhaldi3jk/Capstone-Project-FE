4ebeb6d9b1cc093c67ce1380845badab2e6761e9
Abdallah Alkhaldi <alkhaldi3jk@gmail.com>
2021-12-13 20:36:48 +0300

    please-check-ur-commits

--- components/Pages/RequestItem.js
+++ components/Pages/RequestItem.js
@@ -2,8 +2,7 @@ import React from "react";
 import { Text } from "react-native";
 import { Button, HStack, VStack, Icon } from "native-base";
 import { observer } from "mobx-react";
-import NumericInput from "react-native-numeric-input";
-import RequestStore from "../../stores/RequestStore"
+import RequestStore from "../../stores/RequestStore";
 import { useState } from "react";
 import { Fontisto } from "@expo/vector-icons";
 import DatePicker from "react-native-neat-date-picker";
@@ -22,7 +21,6 @@ const RequestItem = ({ option }) => {
     setShowDatePicker(true);
   };
 
-
   const onCancel = () => {
     // You should close the modal in here
     setShowDatePicker(false);
@@ -35,41 +33,41 @@ const RequestItem = ({ option }) => {
     // The parameter 'date' is a Date object so that you can use any Date prototype method.
     console.log(date.getDate());
 
-  const handleAdd = () => {
-    RequestStore.addRequest(option)
-    // setQuantity(value);
+    const handleAdd = () => {
+      RequestStore.addRequest(option);
+      // setQuantity(value);
+    };
 
+    console.log(option);
+    return (
+      <HStack w="100%" alignItems="center" space="3">
+        <VStack>
+          <Text>{option.name}</Text>
+          <Button
+            leftIcon={<Icon as={Fontisto} name="date" size="sm" />}
+            color="#4f59b1"
+            mt="1"
+            backgroundColor="#4f59b1"
+            size="20"
+            title={"Pick a date"}
+            onPress={openDatePicker}
+          />
+          <DatePicker
+            isVisible={showDatePicker}
+            mode={"single"}
+            onCancel={onCancel}
+            onConfirm={onConfirm}
+            color="F1F2F9"
+            backgroundColor="#4F59B1"
+            headerColor="#4f59b1"
+            colorOptions={colorOptions}
+          />
+
+          <Button>{option.date}</Button>
+        </VStack>
+      </HStack>
+    );
   };
-
-  console.log(option);
-  return (
-    <HStack w="100%" alignItems="center" space="3">
-      <VStack>
-        <Text>{option.name}</Text>
-        <Button
-          leftIcon={<Icon as={Fontisto} name="date" size="sm" />}
-          color="#4f59b1"
-          mt="1"
-          backgroundColor="#4f59b1"
-          size="20"
-          title={"Pick a date"}
-          onPress={openDatePicker}
-        />
-        <DatePicker
-          isVisible={showDatePicker}
-          mode={"single"}
-          onCancel={onCancel}
-          onConfirm={onConfirm}
-          color="F1F2F9"
-          backgroundColor="#4F59B1"
-          headerColor="#4f59b1"
-          colorOptions={colorOptions}
-        />
-
-        <Button>{option.date}</Button>
-      </VStack>
-    </HStack>
-  );
 };
 
 export default observer(RequestItem);
