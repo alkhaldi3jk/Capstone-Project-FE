82b102723a4db820b5ee9c988afcaffe7cd9bf86
Abdallah Alkhaldi <alkhaldi3jk@gmail.com>
2021-12-14 15:02:01 +0300

    working-on-it

--- components/Pages/ServiceDetail.js
+++ components/Pages/ServiceDetail.js
@@ -20,14 +20,9 @@ import {
   Text,
 } from "native-base";
 
-
-import React from "react";
-
-import { Image } from "react-native";
 import { baseURL } from "../../stores/instance";
 import RequestList from "./RequestList";
 import RequestUpdate from "./RequestUpdate";
-
 
 function ServiceDetail({ route, navigation }) {
   const colorOptions = {
@@ -257,7 +252,6 @@ function ServiceDetail({ route, navigation }) {
         </Center>
       </View>
     </ScrollView>
-
   );
 }
 
