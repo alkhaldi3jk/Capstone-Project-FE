61176d3ba7cc2be9a6b04255249a16fcdfecee5f
Ghadahb <ghada.dxb@gmail.com>
2021-12-14 20:34:08 +0300

    services-changes

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -20,7 +20,7 @@ const RootNavigator = () => {
   const { Navigator, Screen } = createStackNavigator();
 
   return (
-    <Navigator initialRouteName="ProfileUpdate">
+    <Navigator initialRouteName="AboutUs">
       {/* <Screen
             name="UserItem"
             component={UserItem}
