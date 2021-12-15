8d77c24bb28839d99136ae7268cf70f1f91bab54
Ghadahb <ghada.dxb@gmail.com>
2021-12-15 17:22:50 +0300

    fixing-UI

--- stores/instance.js
+++ stores/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-export const baseURL = "http://192.168.43.3:8080"; // Abdullah
+// export const baseURL = "http://192.168.43.3:8080"; // Abdullah
 
 // export const baseURL = "http://172.20.10.9:8080"; // Abdullah
 
@@ -14,7 +14,7 @@ export const baseURL = "http://192.168.43.3:8080"; // Abdullah
 
 // export const baseURL = "http://192.168.8.149:8080"; // Amnah
 
-// export const baseURL = "http://172.20.10.3:8080"; // Ghadah
+export const baseURL = "http://172.20.10.3:8080"; // Ghadah
 
 export const instance = axios.create({
   baseURL: `${baseURL}/api`,
